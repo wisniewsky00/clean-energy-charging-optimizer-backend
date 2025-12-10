3051b5a62eb3e06cafd7325f33825ef46bf07f89
wisniewsky00 <krystian.wisniewski2906@gmail.com>
2025-12-10 14:24:20 +0100

    chore: add initial project structure

---
+++ src/main/java/com/github/wisniewsky00/clean_energy_charging_optimizer/CleanEnergyChargingOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.github.wisniewsky00.clean_energy_charging_optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanEnergyChargingOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanEnergyChargingOptimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/wisniewsky00/clean_energy_charging_optimizer/CleanEnergyChargingOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wisniewsky00.clean_energy_charging_optimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanEnergyChargingOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
